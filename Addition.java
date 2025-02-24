7ad786ac9d3783e0d5339abd0fdaf6e10f0e594b
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2025-02-24 13:07:55 +0530

    Add files via upload

--- Addition.java
+++ Addition.java
@@ -1,15 +1,13 @@
+import java.util.*;
 
-import java.util.Scanner;
-public class Addition {
-    public static  void main(String arg[])
-    {
-        Scanner s=new Scanner(System.in);
-
-        System.out.println("Enter 2 numbers");
-        int a=s.nextInt();
-        int b=s.nextInt();
-        int c=a+b;
-        System.out.println("Result is :"+c);
-    }
-    
-}
+public class Addition{
+	public static void main(String arg[]){
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.print("Enter the value of a:");
+		int a=sc.nextInt();
+		System.out.print("Enter the value of b:");
+		int b=sc.nextInt();
+		System.out.println("Sum is:"+(a+b));
+		}	
+	}
