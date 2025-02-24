13ff077d0c872f1435d7381c045101d44196631a
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2025-02-24 14:17:15 +0530

    Inverse star pattern

---
+++ s2mca/Basics/set3/InversePyramid.java
@@ -0,0 +1,14 @@
+class InversePyramid{
+	public static void main(String arg[]){
+		int n=Integer.parseInt(arg[0]);
+		
+		for(int i=0;i<=n;i++){
+			for(int j=i;j<n;j++){
+				System.out.print("*");
+				}
+				System.out.println(" ");
+					}
+		
+		
+		}
+	}
