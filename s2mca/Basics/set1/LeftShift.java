060f2c56619c18cd8928adfbdc4d54a61ac06a05
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2025-02-24 13:15:13 +0530

    LeftShift operation

---
+++ s2mca/Basics/set1/LeftShift.java
@@ -0,0 +1,13 @@
+public class LeftShift{
+	
+  public static void main(String arg[]){
+  	int a=10;
+	
+	System.out.println("True value of a :"+a);	  	
+  	System.out.println("Left shift by 2:"+(a<<2));  	
+  	System.out.println("Left shift by 3:"+(a<<3));  	
+  	System.out.println("Left shift by 4:"+(a<<4));
+  	}	
+	
+	
+	}
