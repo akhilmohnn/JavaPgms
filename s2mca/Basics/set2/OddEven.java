f87c36269f4e95385d7dcee5ad988467b3277129
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2025-02-24 13:17:57 +0530

    Odd Even number

---
+++ s2mca/Basics/set2/OddEven.java
@@ -0,0 +1,16 @@
+
+public class OddEven{
+	
+  public static void main(String arg[]){
+  	int a=Integer.parseInt(arg[0]);
+  	
+  	if(a%2==0){
+  	System.out.println(a+" is even");
+  		}
+  	else{
+  	System.out.println(a+" is odd");
+  		}
+  		}
+	
+	
+	}
