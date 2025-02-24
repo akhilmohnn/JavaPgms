9ebb5d393a7ed477533f78fee426810d00dd1d63
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2025-02-24 13:16:59 +0530

    Arithmetic operations -->extended

---
+++ s2mca/Basics/set2/Arithmetic.java
@@ -0,0 +1,20 @@
+
+
+public class Arithmetic{
+	public static void main(String arg[]){
+		
+		System.out.println("Arithmetic operations");
+		int a=Integer.parseInt(arg[0]);
+		int b=Integer.parseInt(arg[1]);
+		System.out.println("Value of a is:"+a);
+		System.out.println("Value of b is:"+b);
+		
+		
+		System.out.println("Sum is:"+(a+b));
+		System.out.println("Difference is:"+(a-b));
+		System.out.println("Product is:"+(a*b));
+		System.out.println("Remainder is:"+(a%b));
+		
+		
+		}	
+	}
