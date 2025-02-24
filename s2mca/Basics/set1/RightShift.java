b8c76ed3331fd7aaa99a035f922cc5d771e36427
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2025-02-24 13:16:10 +0530

    RightShift operation

---
+++ s2mca/Basics/set1/RightShift.java
@@ -0,0 +1,13 @@
+public class RightShift{
+	
+  public static void main(String arg[]){
+  	int a=10;
+	
+	System.out.println("True value of a :"+a);	  	
+  	System.out.println("Right shift by 2:"+(a>>2));  	
+  	System.out.println("Right shift by 3:"+(a>>3));  	
+  	System.out.println("Right shift by 4:"+(a>>4));
+  	}	
+	
+	
+	}
