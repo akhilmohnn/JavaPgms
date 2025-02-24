9113f84ac76b5cc8ea0aa6faba7462b81755d625
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2025-02-24 13:19:19 +0530

    Positive || Negative

---
+++ s2mca/Basics/set2/PosNeg.java
@@ -0,0 +1,18 @@
+public class PosNeg{
+	
+  public static void main(String arg[]){
+  	int a=Integer.parseInt(arg[0]);
+  	
+  	if(a>0 && a!=0){
+  	System.out.println(a+" is positive");
+  		}
+  	else if((a<0 && a!=0)){
+  	System.out.println(a+" is negative");
+  		}
+  	else{
+  	System.out.println(a+" is zero!!!");
+  	}	
+  		}
+	
+	
+	}
