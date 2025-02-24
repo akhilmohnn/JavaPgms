1b332d160e24e7443009c4519c35685e59be2ccb
Akhil Mohanan <99524393+akhilmohnn@users.noreply.github.com>
2025-02-24 13:13:59 +0530

    Create DefValue.java

---
+++ s2mca/Basics/set1/DefValue.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class DefValue{
+	
+	static int a;
+	static float b;
+	static char c;
+	static boolean d;
+	
+	public static void main(String arg[]){
+		System.out.println("Value of a is:"+a);
+		System.out.println("Value of b is:"+b);
+		System.out.println("Value of c is:"+c);
+		System.out.println("Value of d is:"+d);
+		
+		}	
+	}
