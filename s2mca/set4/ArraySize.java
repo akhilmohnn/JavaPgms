1f0dd61142c4592612d2143888bfa25be98dd2b1
Akhil Mohanan <akhilmohnn@outlook.com>
2025-03-17 01:01:11 +0530

    Array size Array.size(arr)

---
+++ s2mca/set4/ArraySize.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+class ArraySize{
+
+public static void main(String args[]){
+
+Scanner sc=new Scanner(System.in);
+
+System.out.print("Enter the size of the array:");
+int n=sc.nextInt();
+int arr[]=new int[n];
+
+System.out.println("Size of array is:"+arr.length);
+	
+
+	
+	}
+	
+	}
